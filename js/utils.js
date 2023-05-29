8060ab634a7914d7754b7ed1b5cc2414637bdadc
seungho-hub <63999413+kangseungho-hub@users.noreply.github.com>
2023-05-29 18:42:52 +0900

    refactor

--- js/utils.js
+++ js/utils.js
@@ -1,37 +1,43 @@
-function rectangularCollision({ rectangle1, rectangle2 }) {
-  return (
-    rectangle1.attackBox.position.x + rectangle1.attackBox.width >=
-      rectangle2.position.x &&
-    rectangle1.attackBox.position.x <=
-      rectangle2.position.x + rectangle2.width &&
-    rectangle1.attackBox.position.y + rectangle1.attackBox.height >=
-      rectangle2.position.y &&
-    rectangle1.attackBox.position.y <= rectangle2.position.y + rectangle2.height
-  )
+function isCollapsed(rectangle1, rectangle2) {
+  const {
+    position: { x: x1, y: y1 },
+    width: w1,
+    height: h1,
+  } = rectangle1.attackBox;
+  const {
+    position: { x: x2, y: y2 },
+    width: w2,
+    height: h2,
+  } = rectangle2;
+
+  return x1 + w1 >= x2 && x1 <= x2 + w2 && y1 + h1 >= y2 && y1 <= y2 + h2;
 }
 
-function determineWinner({ player, enemy, timerId }) {
-  clearTimeout(timerId)
-  document.querySelector('#displayText').style.display = 'flex'
+function determineWinner(player, enemy, timerId) {
+  clearTimeout(timerId);
+  const displayText = document.querySelector("#displayText");
+  displayText.style.display = "flex";
+
   if (player.health === enemy.health) {
-    document.querySelector('#displayText').innerHTML = '무승부..'
+    displayText.innerHTML = "무승부..";
   } else if (player.health > enemy.health) {
-    document.querySelector('#displayText').innerHTML = '겐지 승리!'
-  } else if (player.health < enemy.health) {
-    document.querySelector('#displayText').innerHTML = '사무라이 승리!'
+    displayText.innerHTML = "킹 승리!";
+  } else {
+    displayText.innerHTML = "마법사 승리!";
   }
 }
 
-let timer = 60
-let timerId
+let timer = 60;
+let timerId;
+
 function decreaseTimer() {
   if (timer > 0) {
-    timerId = setTimeout(decreaseTimer, 1000)
-    timer--
-    document.querySelector('#timer').innerHTML = timer
+    timerId = setTimeout(decreaseTimer, 1000);
+    timer--;
+    document.querySelector("#timer").innerHTML = timer;
   }
 
   if (timer === 0) {
-    determineWinner({ player, enemy, timerId })
+    determineWinner(player, enemy, timerId);
   }
 }
