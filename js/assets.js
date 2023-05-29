03c5d45b556cf1373012c13c62561f04d50b5ee6
seungho-hub <63999413+kangseungho-hub@users.noreply.github.com>
2023-05-29 18:41:56 +0900

    add : fighter motion sound effect

--- js/assets.js
+++ js/assets.js
@@ -1,135 +1,80 @@
 const ASSETS = {
-  tazan: {
-    imageSrc: './img/tazan/Idel.png',
-    sprites: {
-      idle: {
-        imageSrc: './img/tazan/Idle.png',
-        framesMax: 10
-      },
-      run: {
-        imageSrc: './img/tazan/Run.png',
-        framesMax: 8
-      },
-      jump: {
-        imageSrc: './img/tazan/Jump.png',
-        framesMax: 3
-      },
-      fall: {
-        imageSrc: './img/tazan/Fall.png',
-        framesMax: 3
-      },
-      attack1: {
-        imageSrc: './img/tazan/Attack1.png',
-        framesMax: 7
-      },
-      takeHit: {
-        imageSrc: './img/tazan/Take Hit.png',
-        framesMax: 3
-      },
-      death: {
-        imageSrc: './img/tazan/Death.png',
-        framesMax: 11
-      }
-    }
-  },
   wizard: {
-    imageSrc: './img/wizard/Idel.png',
+    imageSrc: "./img/wizard/Idel.png",
     sprites: {
       idle: {
-        imageSrc: './img/wizard/Idle.png',
-        framesMax: 6
+        imageSrc: "./img/wizard/Idle.png",
+        framesMax: 6,
       },
       run: {
-        imageSrc: './img/wizard/Run.png',
-        framesMax: 8
+        imageSrc: "./img/wizard/Run.png",
+        framesMax: 8,
       },
       jump: {
-        imageSrc: './img/wizard/Jump.png',
-        framesMax: 2
+        imageSrc: "./img/wizard/Jump.png",
+        framesMax: 2,
       },
       fall: {
-        imageSrc: './img/wizard/Fall.png',
-        framesMax: 3
+        imageSrc: "./img/wizard/Fall.png",
+        framesMax: 3,
       },
       attack1: {
-        imageSrc: './img/wizard/Attack1.png',
-        framesMax: 8
+        imageSrc: "./img/wizard/Attack1.png",
+        framesMax: 8,
       },
       takeHit: {
-        imageSrc: './img/wizard/Take Hit.png',
-        framesMax: 4
+        imageSrc: "./img/wizard/Take Hit.png",
+        framesMax: 4,
       },
       death: {
-        imageSrc: './img/wizard/Death.png',
-        framesMax: 7
-      }
-    }
+        imageSrc: "./img/wizard/Death.png",
+        framesMax: 7,
+      },
+    },
   },
   king: {
-    imageSrc: './img/king/Idel.png',
+    imageSrc: "./img/king/Idel.png",
     sprites: {
       idle: {
-        imageSrc: './img/king/Idle.png',
-        framesMax: 6
+        imageSrc: "./img/king/Idle.png",
+        framesMax: 6,
       },
       run: {
-        imageSrc: './img/king/Run.png',
-        framesMax: 8
+        imageSrc: "./img/king/Run.png",
+        framesMax: 8,
       },
       jump: {
-        imageSrc: './img/king/Jump.png',
-        framesMax: 2
+        imageSrc: "./img/king/Jump.png",
+        framesMax: 2,
       },
       fall: {
-        imageSrc: './img/king/Fall.png',
-        framesMax: 3
+        imageSrc: "./img/king/Fall.png",
+        framesMax: 3,
       },
       attack1: {
-        imageSrc: './img/king/Attack1.png',
-        framesMax: 6
+        imageSrc: "./img/king/Attack1.png",
+        framesMax: 6,
       },
       takeHit: {
-        imageSrc: './img/king/Take Hit.png',
-        framesMax: 4
+        imageSrc: "./img/king/Take Hit.png",
+        framesMax: 4,
       },
       death: {
-        imageSrc: './img/king/Death.png',
-        framesMax: 11
-      }
-    }
+        imageSrc: "./img/king/Death.png",
+        framesMax: 11,
+      },
+    },
   },
-
-  samurai: {
-    imageSrc: './img/samuraiMack/Idel.png',
-    sprites: {
-      idle: {
-        imageSrc: './img/samuraiMack/Idle.png',
-        framesMax: 8
-      },
-      run: {
-        imageSrc: './img/samuraiMack/Run.png',
-        framesMax: 8
-      },
-      jump: {
-        imageSrc: './img/samuraiMack/Jump.png',
-        framesMax: 2
-      },
-      fall: {
-        imageSrc: './img/samuraiMack/Fall.png',
-        framesMax: 2
-      },
-      attack1: {
-        imageSrc: './img/samuraiMack/Attack1.png',
-        framesMax: 6
-      },
-      takeHit: {
-        imageSrc: './img/samuraiMack/Take Hit - white silhouette.png',
-        framesMax: 4
-      },
-      death: {
-        imageSrc: './img/samuraiMack/Death.png',
-        framesMax: 6
-      }
-    }
-  }
-}
+  sounds: {
+    wizard: {
+      attack: document.querySelector("#wizard-attack"),
+      attackHit: document.querySelector("#wizard-attack-hit"),
+      jump: document.querySelector("#wizard-jump"),
+    },
+    king: {
+      attack: document.querySelector("#king-attack"),
+      attackHit: document.querySelector("#king-attack-hit"),
+      jump: document.querySelector("#king-jump"),
+    },
+  },
+};
