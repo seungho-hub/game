9c8b2795cfb236b489123c839702bab37ca00bed
seungho-hub <63999413+kangseungho-hub@users.noreply.github.com>
2023-05-29 17:01:11 +0900

    initial commit

---
+++ js/assets.js
@@ -0,0 +1,135 @@
+const ASSETS = {
+  tazan: {
+    imageSrc: './img/tazan/Idel.png',
+    sprites: {
+      idle: {
+        imageSrc: './img/tazan/Idle.png',
+        framesMax: 10
+      },
+      run: {
+        imageSrc: './img/tazan/Run.png',
+        framesMax: 8
+      },
+      jump: {
+        imageSrc: './img/tazan/Jump.png',
+        framesMax: 3
+      },
+      fall: {
+        imageSrc: './img/tazan/Fall.png',
+        framesMax: 3
+      },
+      attack1: {
+        imageSrc: './img/tazan/Attack1.png',
+        framesMax: 7
+      },
+      takeHit: {
+        imageSrc: './img/tazan/Take Hit.png',
+        framesMax: 3
+      },
+      death: {
+        imageSrc: './img/tazan/Death.png',
+        framesMax: 11
+      }
+    }
+  },
+  wizard: {
+    imageSrc: './img/wizard/Idel.png',
+    sprites: {
+      idle: {
+        imageSrc: './img/wizard/Idle.png',
+        framesMax: 6
+      },
+      run: {
+        imageSrc: './img/wizard/Run.png',
+        framesMax: 8
+      },
+      jump: {
+        imageSrc: './img/wizard/Jump.png',
+        framesMax: 2
+      },
+      fall: {
+        imageSrc: './img/wizard/Fall.png',
+        framesMax: 3
+      },
+      attack1: {
+        imageSrc: './img/wizard/Attack1.png',
+        framesMax: 8
+      },
+      takeHit: {
+        imageSrc: './img/wizard/Take Hit.png',
+        framesMax: 4
+      },
+      death: {
+        imageSrc: './img/wizard/Death.png',
+        framesMax: 7
+      }
+    }
+  },
+  king: {
+    imageSrc: './img/king/Idel.png',
+    sprites: {
+      idle: {
+        imageSrc: './img/king/Idle.png',
+        framesMax: 6
+      },
+      run: {
+        imageSrc: './img/king/Run.png',
+        framesMax: 8
+      },
+      jump: {
+        imageSrc: './img/king/Jump.png',
+        framesMax: 2
+      },
+      fall: {
+        imageSrc: './img/king/Fall.png',
+        framesMax: 3
+      },
+      attack1: {
+        imageSrc: './img/king/Attack1.png',
+        framesMax: 6
+      },
+      takeHit: {
+        imageSrc: './img/king/Take Hit.png',
+        framesMax: 4
+      },
+      death: {
+        imageSrc: './img/king/Death.png',
+        framesMax: 11
+      }
+    }
+  },
+
+  samurai: {
+    imageSrc: './img/samuraiMack/Idel.png',
+    sprites: {
+      idle: {
+        imageSrc: './img/samuraiMack/Idle.png',
+        framesMax: 8
+      },
+      run: {
+        imageSrc: './img/samuraiMack/Run.png',
+        framesMax: 8
+      },
+      jump: {
+        imageSrc: './img/samuraiMack/Jump.png',
+        framesMax: 2
+      },
+      fall: {
+        imageSrc: './img/samuraiMack/Fall.png',
+        framesMax: 2
+      },
+      attack1: {
+        imageSrc: './img/samuraiMack/Attack1.png',
+        framesMax: 6
+      },
+      takeHit: {
+        imageSrc: './img/samuraiMack/Take Hit - white silhouette.png',
+        framesMax: 4
+      },
+      death: {
+        imageSrc: './img/samuraiMack/Death.png',
+        framesMax: 6
+      }
+    }
+  }
+}
